e89e34705717ab3dfea965b931bf7f93fbad4bb3
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-03-05 16:34:04 +0100

    ey

---
+++ 3/script.js
@@ -0,0 +1,8 @@
+const resultElement = document.getElementById('result')
+const input = document.getElementById('input')
+const btn = document.getElementById('btn')
+const sum = input.value
+btn.onclick = function () {
+	const sum = input.value
+	result.textContent = sum
+}
